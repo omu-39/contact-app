b39db87d29d7ef093c6a7e3d47547cd78e8ac46a
Tsukamoto <tanabon2113@gmail.com>
2026-04-25 15:16:09 +0900

    不要な変更の削除

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Models\Contact;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
