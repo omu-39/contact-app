679b8c9c3566c3d9d92a623a85c4fe62f466fdb5
Tsukamoto <tanabon2113@gmail.com>
2026-04-27 15:18:52 +0900

    フォーム入力画面仮完成

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -11,102 +11,144 @@
 <body>
     <x-header />
     <main>
-        <form action="/confirm" method="POST">
+        <h2 class="my-10 text-3xl text-center text-2xl font-bold text-stone-500">Contact</h2>
+        <form action="/confirm" method="POST" class="w-3/5 mx-auto flex flex-col gap-6">
             @csrf
 
             {{-- 名前 --}}
-            <div>
-                <label for="first_name">姓</label>
-                <input type="text" name="first_name" id="first_name" value="{{ old('first_name') }}">
-                @error('first_name')
-                <p>{{ $message }}</p>
-                @enderror
-            </div>
-            <div>
-                <label for="last_name">名</label>
-                <input type="text" name="last_name" id="last_name" value="{{ old('last_name') }}">
-                @error('last_name')
-                <p>{{ $message }}</p>
-                @enderror
+            <div class="flex gap-4 items-center">
+                <label class="w-1/3 text-lg text-stone-500">
+                    お名前 <span class="text-red-500">※</span>
+                </label>
+                <div class="flex-1 flex flex-col gap-1">
+                    <input type="text" name="first_name" id="first_name" value="{{ old('first_name') }}" placeholder="例: 山田" class="bg-gray-100 px-5 py-2">
+                    @error('first_name')
+                    <p class="text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="flex-1 flex flex-col gap-1">
+                    <input type="text" name="last_name" id="last_name" value="{{ old('last_name') }}" placeholder="例: 太郎" class="bg-gray-100 px-5 py-2">
+                    @error('last_name')
+                    <p class="text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
             </div>
 
             {{-- 性別 --}}
-            <div>
-                <label for="gender">性別</label>
-                <input type="radio" name="gender" id="gender" value="1" {{ old('gender') === '1' ? 'checked' : '' }}>男性
-                <input type="radio" name="gender" id="gender" value="2" {{ old('gender') === '2' ? 'checked' : '' }}>女性
-                <input type="radio" name="gender" id="gender" value="3" {{ old('gender') === '3' ? 'checked' : '' }}>その他
+            <div class="flex gap-4 items-center">
+                <label for="gender" class="w-1/3 text-lg text-stone-500">
+                    性別<span class="text-red-500">※</span>
+                </label>
+                <div class="flex-1 flex gap-20">
+                    <label class="flex items-center gap-1 cursor-pointer">
+                        <input type="radio" name="gender" value="1" class="accent-stone-500" checked> 男性
+                    </label>
+                    <label class="flex items-center gap-1 cursor-pointer">
+                        <input type="radio" name="gender" value="2" class="accent-stone-500"> 女性
+                    </label>
+                    <label class="flex items-center gap-1 cursor-pointer">
+                        <input type="radio" name="gender" value="3" class="accent-stone-500"> その他
+                    </label>
+                </div>
                 @error('gender')
-                <p>{{ $message }}</p>
+                <p class="text-red-500">{{ $message }}</p>
                 @enderror
             </div>
 
             {{-- メールアドレス --}}
-            <div>
-                <label for="email">メールアドレス</label>
-                <input type="email" name="email" id="email" value="{{ old('email') }}">
-                @error('email')
-                <p>{{ $message }}</p>
-                @enderror
+            <div class="flex gap-4 items-center">
+                <label for="email" class="w-1/3 text-lg text-stone-500">
+                    メールアドレス<span class="text-red-500">※</span>
+                </label>
+                <div class="flex-1 flex flex-col gap-1">
+                    <input type="email" name="email" id="email" value="{{ old('email') }}" placeholder="例: test@example.com" class="bg-gray-100 px-5 py-2">
+                    @error('email')
+                    <p class="text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
             </div>
 
             {{-- 電話番号 --}}
-            <div>
-                <label for="tel">電話番号</label>
-                <input type="text" name="tel1" id="tel1" value="{{ old('tel1') }}"> -
-                <input type="text" name="tel2" id="tel2" value="{{ old('tel2') }}"> -
-                <input type="text" name="tel3" id="tel3" value="{{ old('tel3') }}">
-                @error('tel')
-                <p>{{ $message }}</p>
-                @enderror
+            <div class="flex gap-4 items-center">
+                <label for="tel" class="w-1/3 text-lg text-stone-500">
+                    電話番号<span class="text-red-500">※</span>
+                </label>
+                <div class="flex-1 flex flex-col gap-1">
+                    <div class="flex-1 flex justify-between">
+                        <input type="text" name="tel1" id="tel1" value="{{ old('tel1') }}" placeholder="080" class="bg-gray-100 px-5 py-2"> -
+                        <input type="text" name="tel2" id="tel2" value="{{ old('tel2') }}" placeholder="1234" class="bg-gray-100 px-5 py-2"> -
+                        <input type="text" name="tel3" id="tel3" value="{{ old('tel3') }}" placeholder="5678" class="bg-gray-100 px-5 py-2">
+                    </div>
+                    @error('tel')
+                    <p class="text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
             </div>
 
             {{-- 住所 --}}
-            <div>
-                <label for="address">住所</label>
-                <input type="text" name="address" id="address" value="{{ old('address') }}">
-                @error('address')
-                <p>{{ $message }}</p>
-                @enderror
+            <div class="flex gap-4 items-center">
+                <label for="address" class="w-1/3 text-lg text-stone-500">
+                    住所<span class="text-red-500">※</span>
+                </label>
+                <div class="flex-1 flex flex-col">
+                    <input type="text" name="address" id="address" value="{{ old('address') }}" placeholder="例: 東京都渋谷区千駄ヶ谷1-2-3" class="bg-gray-100 px-5 py-2">
+                    @error('address')
+                    <p class="text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
             </div>
 
             {{-- 建物名 --}}
-            <div>
-                <label for="building">建物名</label>
-                <input type="text" name="building" id="building" value="{{ old('building') }}">
-                @error('building')
-                <p>{{ $message }}</p>
-                @enderror
+            <div class="flex gap-4 items-center">
+                <label for="building" class="w-1/3 text-lg text-stone-500">
+                    建物名
+                </label>
+                <div class="flex-1 flex flex-col">
+                    <input type="text" name="building" id="building" value="{{ old('building') }}" placeholder="例: 千駄ヶ谷マンション101" class="bg-gray-100 px-5 py-2">
+                    @error('building')
+                    <p class="text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
             </div>
 
             {{-- お問い合わせの種類 --}}
-            <div>
-                <label for="category_id">お問い合わせの種類</label>
-                <select name="category_id" id="category_id">
-                    <option value="">選択してください</option>
-                    @foreach ($categories as $category)
+            <div class="flex gap-4 items-center">
+                <label for="category_id" class="w-1/3 text-lg text-stone-500">
+                    お問い合わせの種類<span class="text-red-500">※</span>
+                </label>
+                <div class="flex-1 flex flex-col">
+                    <select name="category_id" id="category_id" class="w-1/2 bg-gray-100 px-4 py-2 text-gray-500">
+                        <option value="">選択してください</option>
+                        @foreach ($categories as $category)
                         <option value="{{ $category->id }}"
                             {{ old('category_id') == $category->id ? 'selected' : '' }}>
                             {{ $category->content }}
                         </option>
-                    @endforeach
-                </select>
-                @error('category_id')
-                <p>{{ $message }}</p>
-                @enderror
+                        @endforeach
+                    </select>
+                    @error('category_id')
+                    <p class="text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
             </div>
 
             {{-- お問い合わせ内容 --}}
-            <div>
-                <label for="detail">お問い合わせ内容</label>
-                <textarea name="detail" id="detail">{{ old('detail') }}</textarea>
-                @error('detail')
-                <p>{{ $message }}</p>
-                @enderror
+            <div class="flex gap-4 items-start">
+                <label for="detail" class="w-1/3 text-lg text-stone-500">
+                    お問い合わせ内容<span class="text-red-500">※</span>
+                </label>
+                <div class="flex-1 flex flex-col">
+                    <textarea name="detail" id="detail" class="h-32 bg-gray-100 px-5 py-3" placeholder="お問い合わせ内容をご記載ください">{{ old('detail') }}</textarea>
+                    @error('detail')
+                    <p class="text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
             </div>
 
             {{-- ボタン --}}
-            <button type="submit">確認画面</button>
+            <button type="submit" class=" m-auto my-6 bg-stone-600/80 text-white py-2 px-8">
+                確認画面
+            </button>
         </form>
     </main>
 </body>
