1af06034b42e1b68d21f40b67bd3f3c208498e16
Tsukamoto <tanabon2113@gmail.com>
2026-04-27 12:06:19 +0900

    headerコンポーネントの修正

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    @vite('resources/css/app.css')
+    <title>Contact</title>
 </head>
 
 <body>
